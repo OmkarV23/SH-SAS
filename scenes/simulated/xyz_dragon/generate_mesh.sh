a933513b4aec181fb6b7afc07edae1031a5e54e3
OmkarV23 <omkarvengurlekar66@gmail.com>
2025-08-21 18:25:06 -0700

    remaining files

---
+++ scenes/simulated/xyz_dragon/generate_mesh.sh
@@ -0,0 +1,15 @@
+python ../../../inr_reconstruction/upsample_network_with_input_args.py \
+  --exp_name $2 \
+  --experiment_dir ./ \
+  --inr_config ./nbp_config.json \
+  --num_layers 2 \
+  --num_neurons 32 \
+  --output_scene_file_name final_upsampled_scene \
+  --output_dir_name reconstructed_scenes \
+  --system_data $1 \
+  --normalize_scene_dims \
+  --sf 2 \
+  --max_voxels 15000 \
+  --view_model \
+  --threshold 0.1 \
+  --model_path $3
--- scenes/simulated/xyz_dragon/simulate_waveforms.sh
+++ scenes/simulated/xyz_dragon/simulate_waveforms.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# old sim
-
 python ../../../airsas/reconstruct_from_numpy.py \
   --input_config $1 \
   --output_config ./system_data_20db.pik \
@@ -13,17 +11,3 @@ python ../../../airsas/reconstruct_from_numpy.py \
   --wfm_part_1 $2 \
   #--fiveK \
   #--correction_term
-
-# # new sim
-
-# python ../../../airsas/reconstruct_from_numpy.py \
-#   --input_config $1 \
-#   --output_config ./system_data_20db_new_sim.pik \
-#   --output_dir ./bp_new_sim \
-#   --object new_sim \
-#   --wfm_dir $2 \
-#   --gpu \
-#   --bin_upsample 20 `# upsample factor`\
-#   --wfm_bw 20 `# bandwidth of the transmit waveform`\
-#   --signal_snr 20 `#signal snr with respect to noise`\
-
